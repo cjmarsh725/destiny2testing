6e4f0affcc6e71393ac7ab92f07ae06e8b15f054
Chris <cjmarsh725@gmail.com>
2019-11-01 18:40:50 -0700

    Added axios and request to api

--- app/src/components/App/App.js
+++ app/src/components/App/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="app">
-      App
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    axios.get('https://www.bungie.net/Platform')
+      .then(result => console.log(result))
+      .catch(error => console.log(error));
+  }
+
+  render() {
+    return (
+      <div className="app">
+        App
+      </div>
+    );
+  }
 }
 
 export default App;
